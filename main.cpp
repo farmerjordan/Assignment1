56350a688fdc457e8ac78f0c90e1f75819053fa1
root <farmer@chapman.edu>
2020-02-20 19:29:42 +0000

    Confirms input file exists in directory

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -12,13 +13,34 @@ int main(){
   cout << "Is this correct? Y or N " << endl;
   cin >> verify;
 
-  while(verify == "N"){
+  while(verify == "N" || verify == "n"){
     cout << "Please enter the name of the text file containing DNA strings." << endl;
     cin >> input_file;
     cout << "You entered: " << input_file << endl;
     cout << "Is this correct? Y or N " << endl;
     cin >> verify;
     }
+
+    ifstream inFile;
+
+    inFile.open(input_file);
+
+    if(!inFile){
+      cout << "Unable to open file: " << input_file << endl;
+      cout << "File not found." << endl;
+      exit(1);
+    }else{
+      cout << "File found. " << endl;
+      cout << "" << endl;
+    }
+
+    string line;
+
+    while(!inFile.eof())
+   {
+     getline(inFile,line);
+     cout << line << endl;
+   }
 
 
 
