f1303c3f0d37bafbc499909a0bfa5a2a8d9039a1
root <farmer@chapman.edu>
2020-02-21 22:05:59 +0000

    Calculates variance of length of DNA strings in file.

--- main.cpp
+++ main.cpp
@@ -37,37 +37,64 @@ int main(){
       cout << "" << endl;
     }
 
-
+    //Initializing variables for use in the while loop that iterates through the input file
     string line;
     int charCount = 0;
     int lineCount = 0;
     float mean = 0;
+
 
     //Testing that I can read in the correct file and print line by line
     while(!inFile.eof()){
      getline(inFile,line);
 
 
-
+     //Making all characters lowercase
      for (string::size_type i = 0; i < line.length(); i++){
        line[i] = tolower (line[i]);
      }
-     line.erase(line.find_last_not_of(" \t\n\r\f\v") + 1); //Removes trailing whitespace to get corret character count
+
+     //Removes trailing whitespace to get correct character count
+     line.erase(line.find_last_not_of(" \t\n\r\f\v") + 1);
+
      charCount = charCount + line.size();
-
-     cout << line << endl;
-
      lineCount += 1;
 
-     mean = (float)charCount / (float)lineCount;
+    }
 
+    //Calculation of mean number of characters in a DNA string
+    mean = (float)charCount / (float)lineCount;
+
+
+
+
+    int lengthString = 0;
+    float varianceNumerator = 0;
+    float variance = 0;
+
+    inFile.clear();
+    inFile.seekg(0, ios::beg);
+    //Calculating variance doing it after previous while loop so that the mean was available
+    while(!inFile.eof()){
+      getline(inFile,line);
+
+      line.erase(line.find_last_not_of(" \t\n\r\f\v") + 1);
+
+      lengthString = line.size();
+
+      varianceNumerator += ((float)lengthString - (float)mean) * ((float)lengthString - (float)mean);
 
     }
+
+    variance = (float)varianceNumerator / (float)lineCount;
+
+
 
   //All of this will later be outputted to the output file
   cout << "Total number of characters in the file: " << charCount << endl;
   cout << "Total number of DNA strings in the file: " << lineCount << endl;
   cout << "The mean length of the DNA strings is: " << mean << endl;
+  cout << "The variance of the length of the DNA strings is: " << variance << endl;
 
 
 }
